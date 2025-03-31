b45d1a7b77e57af21ae8db0b653d36318f7e83b8
snufaffu <alex.hong3350@gmail.com>
2025-03-30 21:56:47 -0400

    physics time

--- silly.cpp
+++ silly.cpp
@@ -9,7 +9,6 @@
 #include "Field.h"
 #include <algorithm>
 
-
 using namespace std;
 
 class Table {
@@ -19,7 +18,7 @@ class Table {
   int numData = 0;
   vector<ColumnType> coltypes;
   unordered_map<string, int> colIndex;
-  vector<Field> rows;
+  vector<vector<Field>> rows;
   int totalRows = 0;
 };
 
@@ -28,23 +27,41 @@ class Database {
   unordered_map <string, Table> database;
 };
 
-struct lessThan {
-  bool operator()(ColumnType a, ColumnType b){
-    return a < b;
+
+class lessThan {
+  bool operator()(const vector<Field> & vec, Field val) const {
+    for (auto& v : vec) {
+      if(v < val){
+        return true;
+      }
+    }
+    return false;
   }
 };
 
-struct Equal {
-  bool operator()(ColumnType a, ColumnType b){
-    return a == b;
+
+class equalTo {
+  bool operator()(const vector<Field> & vec, Field val) const {
+    for (auto& v : vec) {
+      if(v == val){
+        return true;
+      }
+    }
+    return false;
   }
 };
 
-struct greaterThan {
-  bool operator()(ColumnType a, ColumnType b){
-    return a > b;
+class greaterThan {
+  bool operator()(const vector<Field> & vec, Field val) const {
+    for (auto& v : vec) {
+      if(v > val){
+        return true;
+      }
+    }
+    return false;
   }
 };
+
 
 //global variables
 Database db;
@@ -222,42 +239,51 @@ int main(int argc, char** argv) {
         " from position " << db.database[tableName].totalRows << " to " 
         << numRows + db.database[tableName].totalRows - 1 << "\n";
 
+
+
         db.database[tableName].totalRows = db.database[tableName].totalRows + numRows;
-        db.database[tableName].rows.reserve(numRows);
+
+        db.database[tableName].rows.reserve(numRows + db.database[tableName].totalRows);
+
+
         // cout << db.database[tableName].cols;
         for(int i = 0; i < numRows; ++i){
+          db.database[tableName].rows.emplace_back();
+          db.database[tableName].rows.back().reserve(db.database[tableName].cols);
           for(int j = 0; j < db.database[tableName].cols; ++j){
+            
+
             switch(db.database[tableName].coltypes[j]){
 
               case  ColumnType::Bool : 
                 cin >> junk;
-                db.database[tableName].rows.emplace_back(junk == "true");
+                db.database[tableName].rows[i].emplace_back(junk == "true");
                 ++db.database[tableName].numData;
-                // cout << db.database[tableName].rows.back() << '\n';
+                cerr << db.database[tableName].rows[i].back() << '\n';
               break;
               
 
               case ColumnType::Double : 
                 cin >> junk;
-                db.database[tableName].rows.emplace_back(stod(junk));
+                db.database[tableName].rows[i].emplace_back(stod(junk));
                 ++db.database[tableName].numData;
-                // cout << db.database[tableName].rows.back() << '\n';
+                cerr << db.database[tableName].rows[i].back() << '\n';
               break;
               
 
               case ColumnType::String : 
                 cin >> junk;
-                db.database[tableName].rows.emplace_back(junk);
+                db.database[tableName].rows[i].emplace_back(junk);
                 ++db.database[tableName].numData;
-                // cout << db.database[tableName].rows.back() << '\n';
+                cerr << db.database[tableName].rows[i].back() << '\n';
               break;
               
 
               case ColumnType::Int : 
                 cin >> junk;
-                db.database[tableName].rows.emplace_back(stoi(junk));
+                db.database[tableName].rows[i].emplace_back(stoi(junk));
                 ++db.database[tableName].numData;
-                // cout << db.database[tableName].rows.back() << '\n';
+                cerr << db.database[tableName].rows[i].back() << '\n';
               break;
               
             }
@@ -296,7 +322,7 @@ int main(int argc, char** argv) {
         if(junk[0] == 'W'){
           cout << "come back later";
           break;
-        } // print where
+        } // not this far yet
 
         if(junk[0] == 'A'){
           for(int i = 0; i < N; ++i){
@@ -310,7 +336,7 @@ int main(int argc, char** argv) {
             for(int i = 0; i < db.database[tableName].totalRows; ++i){
               for(int j = 0; j < N; ++j){
                 index = indexList[j];
-                  cout << db.database[tableName].rows[i*db.database[tableName].cols+index];
+                  cout << db.database[tableName].rows[i][index];
                   cout << ' ';
               }
               cout << '\n';
@@ -325,19 +351,17 @@ int main(int argc, char** argv) {
       //print
 
       case 'D' : {
-        vector<Field> testData;
         string junk;
         string inputname;
         char op;
         string val;
         int index;
-        // testData.reserve(1);
       
         cin >> junk >> tableName >> junk >> inputname >> op;
 
 
         if(doesTableExist(tableName) == false){
-          cout << "Error during DELETE: " << tableName << " does not name a table in the database\n";
+          cout << "Error during INSERT: " << tableName << " does not name a table in the database\n";
           break;
         } // error checking
         
@@ -346,7 +370,7 @@ int main(int argc, char** argv) {
         switch(db.database[tableName].coltypes[index]){
           case ColumnType::String : {
             cin >> val;
-            testData.emplace_back(val);
+            cout << "string\n";
           break;
         }
 
@@ -354,19 +378,19 @@ int main(int argc, char** argv) {
           case ColumnType::Double : {
             cin >> val;
             cout << "double\n";
-            testData.emplace_back(stod(val));
           break;
           }
 
           case ColumnType::Int : {
             cin >> val;
-            testData.emplace_back(stoi(val));
+            cout << "int\n";
           break;
           }
 
           case ColumnType::Bool : {
             cin >> val;
-            testData.emplace_back(val == "true");
+            bool boolVal = (val == "true");
+            cout << boolVal << '\n';
           break;
           }
 
@@ -374,27 +398,21 @@ int main(int argc, char** argv) {
 
         switch(op) {
           case '=' :
-  
-          for(int i = 0; i < db.database[tableName].totalRows; ++i){
-            if(db.database[tableName].rows[i*db.database[tableName].cols+index] == testData[0]){
-              
-            }
-          }
-          
-
+          // db.database[tableName].rows.erase(remove_if(db.database[tableName].rows.begin(), db.database[tableName].rows.end(), equalTo(db.database[tableName].rows, val)));
+          break;
         }
 
         break;
       }
       //delete
-
+      
       case 'J' :
-
+        cout << "joining\n";
         break;
       //join
       
       case 'G' :
-
+        cout << "generating\n";
         break;
       //generate
 
